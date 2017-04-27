1d88a8c40008a89b767c20d80fa4fdac4b594a1f
LZ <luyizk@gmail.com>
2017-04-27 14:17:14 -0400

    implement render data rows

--- client/js/table-view.js
+++ client/js/table-view.js
@@ -1,5 +1,8 @@
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, 
+	      createTR,
+	      createTH,
+	      createTD } = require('./dom-util');
 
 class TableView {
 	constructor(model) {
@@ -13,10 +16,12 @@ class TableView {
 
 	initDomReferences() {
 		this.headerRowEl = document.querySelector('THEAD TR');
+		this.sheetBodyEl = document.querySelector('TBODY');
 	}
 
 	renderTable() {
 		this.renderTableHeader();
+		this.renderTableBody();
 	}
 	
 	renderTableHeader() {
@@ -26,6 +31,28 @@ class TableView {
 		getLetterRange('A', this.model.numCols)
 		  .map(colLabel => createTH(colLabel))
 		  .forEach(th => this.headerRowEl.appendChild(th));
+	}
+
+	renderTableBody() {
+		const fragment = document.createDocumentFragment();
+		for (let row = 0; row < this.model.numRows; row++) {
+			// create each row
+			const tr = createTR();
+			for (let col = 0; col < this.model.numCols; col++) {
+				const position = { col: col, row: row };
+				const value = this.model.getValue(position);
+				// create each standard cell
+				const td = createTD(value);
+				// add each standard cell to row
+				tr.appendChild(td);
+			}
+			// add each row to fragment
+			fragment.appendChild(tr);
+		}
+		// clear sheet body of previous children
+		removeChildren(this.sheetBodyEl);
+		// add fragment to sheet body
+		this.sheetBodyEl.appendChild(fragment);
 	}
 }
 
