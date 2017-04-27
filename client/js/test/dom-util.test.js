7282970c6dae84451640e0c82d0884a5db990b8f
LZ <luyizk@gmail.com>
2017-04-26 23:40:03 -0400

    add tests for element creation DOM utility method

--- client/js/test/dom-util.test.js
+++ client/js/test/dom-util.test.js
@@ -1,20 +1,57 @@
-const { removeChildren } = require('../dom-util');
+const { createTH,
+				createTR,
+				createTD,
+				removeChildren } = require('../dom-util');
 
 describe('dom-util', () => {
-	it('removes one child', () => {
-		// set up initial state
-		const parent = document.createElement('DIV');
-		const child = document.createElement('STRONG');
-		parent.appendChild(child);
 
-		// inspect initial state
-		expect(parent.childNodes.length).toBe(1);
-		expect(parent.childNodes[0]).toBe(child);
+	describe('DOM creation functions', () => {
+		describe('createTH', () => {
+			it('produces valid TH element', () => {
+				const el = createTH();
+				expect(el.tagName).toBe('TH');
+			});
 
-		// execute code under test
-		removeChildren(parent);
+			it('sets the text of the TH', () => {
+				const text = 'Hello world!';
+				const el = createTH(text);
+				expect(el.textContent).toEqual(text);
+			});
 
-		// inspect resulting state
-		expect(parent.childNodes.length).toBe(0);
+		});
+
+		describe('createTR', () => {
+			it('produces valid TR element', () => {
+				const el = createTR();
+				expect(el.tagName).toBe('TR');
+			});
+		});
+
+		describe('createTD', () => {
+			it('produces valid TD element', () => {
+				const el = createTD();
+				expect(el.tagName).toBe('TD');
+			});
+		});
+
+	});
+
+	describe('removeChildren()', () => {
+		it('removes one child', () => {
+			// set up initial state
+			const parent = document.createElement('DIV');
+			const child = document.createElement('STRONG');
+			parent.appendChild(child);
+
+			// inspect initial state
+			expect(parent.childNodes.length).toBe(1);
+			expect(parent.childNodes[0]).toBe(child);
+
+			// execute code under test
+			removeChildren(parent);
+
+			// inspect resulting state
+			expect(parent.childNodes.length).toBe(0);
+		});
 	});
 });
