959f916f6da3e57d2c82fe3bde3f3198882f48f7
LZ <luyizk@gmail.com>
2017-04-29 23:16:21 -0400

    add table model tests for row highlight

--- client/js/test/table-model.test.js
+++ client/js/test/table-model.test.js
@@ -17,6 +17,51 @@ describe('table-model', () => {
 		expect(model.getValue(location)).toBe('foo');
 	});
 
+	it('can set then get a color', () => {
+		// set up the initial state
+		const model = new TableModel();
+		const location = { col: 5, row: 3 };
+
+		// inspect the initial state
+		expect(model.getColor(location)).toBeUndefined();
+
+		// execute code under test
+		model.setColor(location, "yellow");
+
+		// inspect the resulting state
+		expect(model.getColor(location)).toBe("yellow");
+	});
+
+	it('initially only has current cell highlighted', () => {
+		// set up the initial state
+		const model = new TableModel(2, 3);
+
+		// inspect the initial state
+		expect(model.rowHighlighted).toBe("none");
+		expect(model.colHighlighted).toBe("none");
+		expect(model.currentCellHighlighted).toBe(true);
+	});
+
+	it('can highlight a row given an input row', () => {
+		// set up the initial state
+		const model = new TableModel(2, 3);
+
+		// inspect the initial state
+		expect(model.rowHighlighted).toBe("none");
+    expect(Object.keys(model.colors).length).toBe(0);
+
+    // execute code under test
+    model.highlightRow(2);
+
+    // inspect the resulting state
+    expect(model.getColor({ col: 0, row: 1 })).toBe("yellow");
+    expect(model.getColor({ col: 1, row: 1 })).toBe("yellow");
+    expect(Object.keys(model.colors).length).not.toBe(0);
+    expect(Object.keys(model.colors).length).toBe(2);
+    expect(model.rowHighlighted).toBe(2);
+    
+	});
+
 	it('can compute the sum of a column with some integer inputs', () => {
 		// set up the initial state
 		const model = new TableModel();
